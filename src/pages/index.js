1711ec385b613d59774a6cbaabf6bc48f353e885
Tia Esguerra <msksfo@gmail.com>
2020-04-26 22:08:52 -0700

    Delete unused code and apply overflow hidden to the video container

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,46 +13,13 @@ import landingPagePoster from "../assets/images/videoStill.png"
 // https://www.youtube.com/watch?v=e3StH0KLKR4
 
 class IndexPage extends Component {
-  _onReady(event) {
-    event.target.mute()
-    event.target.play()
-  }
-
-  _onEnd(event) {
-    event.target.playVideo()
-  }
-
   render() {
-    const videoOptions = {
-      playerVars: {
-        autoplay: 0,
-        controls: 0,
-        rel: 0,
-        showinfo: 0,
-      },
-    }
-
     return (
       <div>
         <Layout>
           {/*  <SEO title="Home" /> */}
 
           <div className={indexStyles.videoContainer}>
-            {/*
-        <video style={{ width: "auto" }} autoplay muted loop controls>
-          <source src={landingPageVideo} type='"video/mp4'></source>
-        </video>
-      */}
-            {/*
-            <YouTube
-              className={indexStyles.video}
-              videoId="e3StH0KLKR4"
-              opt={videoOptions}
-              onReady={this.onReady}
-              onEnd={this.onEnd}
-            />
-          */}
-
             <video autoPlay loop muted poster={landingPagePoster}>
               <source src={landingPageVideo} type="video/mp4" />
             </video>
