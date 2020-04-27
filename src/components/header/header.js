d8a030d2d4454ecf26315953550772e3ece3581d
Tia Esguerra <msksfo@gmail.com>
2020-04-27 10:36:46 -0700

    Add some images for placeholders

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,42 +4,47 @@ import headerStyles from "../header/header.module.scss"
 
 const Header = () => {
   return (
-    <nav>
+    <header className={headerStyles.header}>
       <Link to="/" className={(headerStyles.link, headerStyles.logo)}>
         {" "}
         <span className={headerStyles.title}>Heidi Tsai</span>
         <span className={headerStyles.subtitle}>Historical Keyboardist</span>
       </Link>
-      <ul className={headerStyles.navList}>
-        <li className={headerStyles.navItem}>
-          <Link to="/about" className={headerStyles.link}>
-            {" "}
-            about
-          </Link>
-        </li>
 
-        <li className={headerStyles.navItem}>
-          <Link to="/coaching" className={headerStyles.link}>
-            {" "}
-            coaching
-          </Link>
-        </li>
+      <input type="checkbox" className={headerStyles.navToggle}></input>
 
-        <li className={headerStyles.navItem}>
-          <Link to="/discography" className={headerStyles.link}>
-            {" "}
-            discography
-          </Link>
-        </li>
+      <nav className={headerStyles.headerNav}>
+        <ul className={headerStyles.navList}>
+          <li className={headerStyles.navItem}>
+            <Link to="/about" className={headerStyles.link}>
+              {" "}
+              about
+            </Link>
+          </li>
 
-        <li className={headerStyles.navItem}>
-          <Link to="/contact" className={headerStyles.link}>
-            {" "}
-            contact
-          </Link>
-        </li>
-      </ul>
-    </nav>
+          <li className={headerStyles.navItem}>
+            <Link to="/coaching" className={headerStyles.link}>
+              {" "}
+              coaching
+            </Link>
+          </li>
+
+          <li className={headerStyles.navItem}>
+            <Link to="/discography" className={headerStyles.link}>
+              {" "}
+              discography
+            </Link>
+          </li>
+
+          <li className={headerStyles.navItem}>
+            <Link to="/contact" className={headerStyles.link}>
+              {" "}
+              contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
