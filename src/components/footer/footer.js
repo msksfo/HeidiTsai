524779533999c4395f33b42a891f97e59b2e0c5d
Tia Esguerra <msksfo@gmail.com>
2020-04-26 21:51:51 -0700

    Set up secondary navigation in the footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,10 +1,52 @@
 import React from "react"
+import { Link } from "gatsby"
 import footerStyles from "../footer/footer.module.scss"
 
 const Footer = () => {
   return (
     <div className={footerStyles.wrapper}>
-      <p>© {new Date().getFullYear()} Heidi Tsai. All Rights Reserved</p>
+      <div className={footerStyles.footerNav}>
+        <div>
+          <ul className={footerStyles.footerNavRight}>
+            <li className={footerStyles.navItem}>
+              <Link className={footerStyles.link} to="/about">
+                {" "}
+                about
+              </Link>
+            </li>
+
+            <li className={footerStyles.navItem}>
+              <Link className={footerStyles.link} to="/coaching">
+                {" "}
+                coaching
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div> social links here</div>
+
+        <div>
+          <ul className={footerStyles.footerNavLeft}>
+            <li className={footerStyles.navItem}>
+              <Link className={footerStyles.link} to="/discography">
+                discography
+              </Link>
+            </li>
+
+            <li className={footerStyles.navItem}>
+              <Link className={footerStyles.link} to="/contact">
+                {" "}
+                contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <small className={footerStyles.copyright}>
+        © {new Date().getFullYear()} Heidi Tsai. All Rights Reserved
+      </small>
     </div>
   )
 }
