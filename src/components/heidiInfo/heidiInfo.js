84d22fa03af9db83bfb546113a024a786c9f2ad6
Tia Esguerra <msksfo@gmail.com>
2020-05-01 16:21:09 -0700

    Delete email and phone number at Heidi's request. Replace with generic message

--- src/components/heidiInfo/heidiInfo.js
+++ src/components/heidiInfo/heidiInfo.js
@@ -5,8 +5,10 @@ const HeidiInfo = props => {
   return (
     <div className={heidiInfoStyles.heidiInfo}>
       <p className={heidiInfoStyles.contactHeidi}>Get In Touch</p>
-      <p>{props.email}</p>
-      <tel>{props.phoneNumber}</tel>
+      <p>
+        Fill out the form for more information on working with Heidi, or for any
+        other questions you may have.
+      </p>
     </div>
   )
 }
