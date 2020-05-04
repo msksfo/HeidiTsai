2f1757797b7836ffa953acdc40ceccbb923c9bd9
Tia Esguerra <msksfo@gmail.com>
2020-05-03 21:42:22 -0700

    Finish styling testimonials. Add pseudo element as border between testimonials

--- src/pages/coaching.js
+++ src/pages/coaching.js
@@ -37,7 +37,6 @@ const CoachingPage = () => (
               1-on-1 session by filling out the form on the contact page.
             </p>
           </div>
-
           <div className={coachingStyles.coachingSessionWrapper}>
             <h2 className={coachingStyles.sessionType}>For the Artist in Us</h2>
             <div
@@ -69,9 +68,30 @@ const CoachingPage = () => (
                   </ul>
                 </details>
               </div>
-            </div>
-          </div>
+            </div>{" "}
+            {/* close sessionBackgound */}
+            <ul className={coachingStyles.testimonialsList}>
+              <li>
+                <figure>
+                  "I would recommend it to anybody. There are valuable takeaways
+                  for everyone."
+                  <figcaption>Gary / teacher - Naperville, USA</figcaption>
+                </figure>
+              </li>
 
+              <li>
+                <figure>
+                  "It has significantly opened my mind, heart, eyes and ears to
+                  my surroundings, the people around me, my feelings, desires,
+                  words and actions..."
+                  <figcaption>
+                    Merrie / German and ESL trainer - Hanau, Germany
+                  </figcaption>
+                </figure>
+              </li>
+            </ul>
+          </div>{" "}
+          {/* close coachingSessionWrapper */}
           <div className={coachingStyles.coachingSessionWrapper}>
             <h2 className={coachingStyles.sessionType}>For Professionals</h2>
             <div
@@ -104,8 +124,31 @@ const CoachingPage = () => (
                 </details>
               </div>
             </div>
-          </div>
 
+            <ul className={coachingStyles.testimonialsList}>
+              <li>
+                <figure>
+                  "This is a journey for the whole life, every day! I want to
+                  revisit certain exercises/meditation themes over and over
+                  again, continuously."
+                  <figcaption>
+                    Teddie H. / flutist, photographer - Germany
+                  </figcaption>
+                </figure>
+              </li>
+              <li>
+                <figure>
+                  "The idea of doing the same thing with people from all over
+                  the world enhanced the feeling of connection to the world. It
+                  was a fantastic way to start a day."
+                  <figcaption>
+                    Vaula / baroque dancer, lawyer, harpsichord and life
+                    enthusiast - Seattle, USA
+                  </figcaption>
+                </figure>
+              </li>
+            </ul>
+          </div>
           <div className={coachingStyles.coachingSessionWrapper}>
             <h2 className={coachingStyles.sessionType}>
               A Gift of Deeper Connections
@@ -136,6 +179,28 @@ const CoachingPage = () => (
                 </details>
               </div>
             </div>
+            <ul className={coachingStyles.testimonialsList}>
+              <li>
+                <figure>
+                  "I really appreciated this journey, because this was a gift
+                  that arrived at the right time in my life. I loved going to my
+                  meditation studio several times a month before the lockdown,
+                  but have been reluctant to try the online meditation classes,
+                  so this was perfect. I enjoyed the jounal exercises and
+                  especially the meditation part"
+                  <figcaption>
+                    Jenny L. / artist, designer - Los Angeles, USA
+                  </figcaption>
+                </figure>
+              </li>
+              <li>
+                <figure>
+                  "I loved the first round and wouldn't miss a chance to join
+                  you again"
+                  <figcaption>Lara Zoe Wild</figcaption>
+                </figure>
+              </li>
+            </ul>
           </div>
         </section>
       </div>
