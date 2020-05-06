afe20a4af5bc3178a06d5eafde0930c93c5ea0f2
Tia Esguerra <msksfo@gmail.com>
2020-05-06 11:51:28 -0700

    Make the contactForm component a class component so it can hold state for the form.

--- src/components/contactForm/contactForm.js
+++ src/components/contactForm/contactForm.js
@@ -1,58 +1,119 @@
-import React from "react"
+import React, { Component } from "react"
 import contactFormStyles from "./contactForm.module.scss"
 
 // THIS NEEDS TO BE A CLASS COMPONENT SO IT CAN HOLD STATE FOR THE FORM
-const ContactForm = () => {
-  return (
-    <form className={contactFormStyles.form}>
-      <p>
-        <label for="name">
-          Name:
-          <input type="text" id="name" name="name" required />
-        </label>
-      </p>
+class ContactForm extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: "",
+      email: "",
+      subject: "",
+      phone: "",
+      message: "",
+    }
 
-      <p>
-        <label for="email">
-          Email:
-          <input type="email" id="email" name="email" required />
-        </label>
-      </p>
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
 
-      <p>
-        <label for="subject">Subject:</label>
-        <input
-          type="text"
-          id="subject"
-          name="subject"
-          list="subjects"
-          placeholder="(optional)"
-        />
-        <datalist id="subjects">
-          <option>Embodied Creativity</option>
-          <option>Piano / Harpsichord lessons</option>
-          <option>Book Heidi for a private event</option>
-          <option>Book Heidi & Nabi for a private event</option>
-        </datalist>
-      </p>
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
 
-      <p>
-        <label for="phone">
-          Phone:
-          <input type="tel" id="phone" name="phone" placeholder="(optional)" />
-        </label>
-      </p>
+  handleSubmit(e) {}
 
-      <p className={contactFormStyles.message}>
-        <label>
-          Message:
-          <textarea rows="5" required></textarea>
-        </label>
-      </p>
+  render() {
+    return (
+      <form
+        className={contactFormStyles.form}
+        name="contact"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        method="post"
+        action="/success"
+      >
+        <p>
+          <label htmlFor="name">
+            Name:
+            <input
+              type="text"
+              id="name"
+              value={this.state.name}
+              name="name"
+              onChange={this.handleChange}
+              required
+            />
+          </label>
+        </p>
 
-      <button className={contactFormStyles.submitButton}>Submit</button>
-    </form>
-  )
+        <p>
+          <label htmlFor="email">
+            Email:
+            <input
+              type="email"
+              id="email"
+              onChange={this.handleChange}
+              value={this.state.email}
+              name="email"
+              required
+            />
+          </label>
+        </p>
+
+        <p>
+          <label htmlFor="subject">Subject:</label>
+          <input
+            type="text"
+            id="subject"
+            onChange={this.handleChange}
+            value={this.state.subject}
+            name="subject"
+            list="subjects"
+            placeholder="(optional)"
+          />
+          <datalist id="subjects">
+            <option>Embodied Creativity</option>
+            <option>Piano / Harpsichord lessons</option>
+            <option>Book Heidi for a private event</option>
+            <option>Book Heidi & Nabi for a private event</option>
+          </datalist>
+        </p>
+
+        <p>
+          <label htmlFor="phone">
+            Phone:
+            <input
+              type="tel"
+              id="phone"
+              onChange={this.handleChange}
+              value={this.state.phone}
+              name="phone"
+              placeholder="(optional)"
+            />
+          </label>
+        </p>
+
+        <p className={contactFormStyles.message}>
+          <label htmlFor="message">
+            Message:
+            <textarea
+              rows="5"
+              required
+              onChange={this.handleChange}
+              value={this.state.message}
+              name="message"
+              id="message"
+            ></textarea>
+          </label>
+        </p>
+
+        <button className={contactFormStyles.submitButton}>Submit</button>
+      </form>
+    )
+  }
 }
 
 export default ContactForm
