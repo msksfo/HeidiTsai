0d23568f9b82ae67b5e1d2b5766554a5f3d8880d
Tia Esguerra <msksfo@gmail.com>
2020-05-06 18:59:12 -0700

    Add markup and styles for success page

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout/layout"
 import Head from "../components/head"
+import successStyles from "../styles/success.module.scss"
 
 const Success = () => {
   return (
@@ -9,8 +10,12 @@ const Success = () => {
         {/*  <SEO title="Home" /> */}
         <Head title="Thanks" />
 
-        <div>
-          <p>Your form was successfully delivered.</p>
+        <div className={successStyles.wrapper}>
+          <p>
+            Your message has been sent. I will be in touch with you shortly.
+            <br></br>
+            <span className={successStyles.signature}>Heidi</span>
+          </p>
         </div>
       </Layout>
     </div>
