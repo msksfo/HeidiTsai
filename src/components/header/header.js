d356d5e5a8fd9f1841f85acf0a9ff539af618921
Tia Esguerra <msksfo@gmail.com>
2020-05-13 19:31:19 -0700

    Begin work on mobile navigation.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,80 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 import cs from "classnames"
 import headerStyles from "../header/header.module.scss"
 
+class Header extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      isOpen: false,
+    }
+  }
+
+  render() {
+    return (
+      <header
+        className={cs(headerStyles.header, {
+          [headerStyles.homePageHeader]: this.props.page === "home",
+        })}
+      >
+        <Link to="/" className={(headerStyles.link, headerStyles.logo)}>
+          {" "}
+          <span className={headerStyles.title}>Heidi Tsai</span>
+          <span className={headerStyles.subtitle}>Historical Keyboardist</span>
+        </Link>
+
+        <label
+          htmlFor="nav-toggle"
+          //tabIndex="0"
+          className={headerStyles.navToggleLabel}
+        >
+          <span></span>
+        </label>
+
+        <input
+          type="checkbox"
+          id="nav-toggle"
+          className={headerStyles.navToggle}
+        ></input>
+
+        <nav className={headerStyles.headerNav}>
+          <ul className={headerStyles.navList}>
+            <li className={headerStyles.navItem}>
+              <Link to="/about" className={headerStyles.link}>
+                {" "}
+                about
+              </Link>
+            </li>
+
+            <li className={headerStyles.navItem}>
+              <Link to="/coaching" className={headerStyles.link}>
+                {" "}
+                coaching
+              </Link>
+            </li>
+
+            <li className={headerStyles.navItem}>
+              <Link to="/discography" className={headerStyles.link}>
+                {" "}
+                discography
+              </Link>
+            </li>
+
+            <li className={headerStyles.navItem}>
+              <Link to="/contact" className={headerStyles.link}>
+                {" "}
+                contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    )
+  }
+}
+
+/*
 const Header = props => {
   return (
     <header
@@ -52,5 +124,6 @@ const Header = props => {
     </header>
   )
 }
+*/
 
 export default Header
